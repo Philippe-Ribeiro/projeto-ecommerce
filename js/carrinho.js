5f99e2d3eb0b2e0f7a90bd986902299b230e8daa
Philippe-Ribeiro <philipperibeiropro@gmail.com>
2025-10-06 08:37:01 -0300

    adiciona funcionalidade de salvar Produto no carrinho

---
+++ js/carrinho.js
@@ -0,0 +1,65 @@
+/*
+Objetivo 1 - quando clicar no botão de adicionar ao carrinho:
+    - atualizar o contador
+    - adicionar o produto no localStorage
+    - atualizar a tabela HTML do carrinho
+
+Objetivo 2 - remover produtos do carrinho:
+    - ouvir o botão de deletar
+    - remover do localStorage
+    - atualizar o DOM e o total
+
+Objetivo 3 - atualizar valores do carrinho:
+    - ouvir mudanças de quantidade
+    - recalcular total individual
+    - recalcular total geral
+*/
+
+// Objetivo 1 - quando clicar no botão de adicionar ao carrinho temos que atualizar o contador, adicionar o produto no localStorage e atualizar a tabela HTML do carrinho
+//       passo 1 - pegar os botões de adicionar ao carrinho do html
+
+const botoesAdicionarAoCarrinho = document.querySelectorAll('.adicionar-ao-carrinho');
+
+// passo 2 - adicionar um evento de escuta nesses botões pra quando clicar disparar uma ação
+botoesAdicionarAoCarrinho.forEach(botao => {
+    botao.addEventListener("click", (evento) => {
+        //passo 3 - pega as informações do produto clicado e 
+        // adicionar ao localStorage
+        const elementoProduto = evento.target.closest('.produto');
+        const produtoId = elementoProduto.dataset.id;
+        const ProdutoNome = elementoProduto.querySelector('.nome').textContent;
+        const produtoImagem = elementoProduto.querySelector('img').getAttribute('src');
+        const produtoPreco = parseFloat(elementoProduto.querySelector('.preco').textContent.replace("R$ ", "").replace(".", "").replace(",", "."));
+
+        //buscar a lista de produtos do localStorage
+        const carrinho = obterProdutosDoCarrinho();
+        //testar se o produto já existe no carrinho
+        const existeProduto = carrinho.find(produto => produto.id === produtoId);
+        //se existe produto, incrementar a quantidade
+        if (existeProduto) {
+            existeProduto.quantidade += 1;
+        } else {
+            //se não existe, adicionar o produto com quantidade 1
+            const produto = {
+                id: produtoId,
+                nome: ProdutoNome,
+                imagem: produtoImagem,
+                preco: produtoPreco,
+                quantidade: 1
+            };
+            carrinho.push(produto);
+        }
+
+        salvarProdutosNoCarrinho(carrinho);
+    });
+});
+
+function salvarProdutosNoCarrinho(carrinho) {
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+}
+
+function obterProdutosDoCarrinho() {
+    const produtos = localStorage.getItem("carrinho");
+    return produtos ? JSON.parse(produtos) : [];
+}
+
